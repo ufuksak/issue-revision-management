c17c13eb87483796e66ef0f39afed0488956114f
Ufuk Sakar <ufuk.sakar-e@maf.ae>
2024-07-23 02:29:06 +0300

    feat: issue revision tracking tool

--- docker-entrypoint-initdb.d/1_create_table.sql
+++ docker-entrypoint-initdb.d/1_create_table.sql
@@ -7,3 +7,16 @@ CREATE TABLE issue_db.issues (
   updated_by VARCHAR(100),
   updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
+
+CREATE TABLE issue_db.revisions
+(
+    id          INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    state       VARCHAR(100) NOT NULL,
+    title       VARCHAR(100) NOT NULL,
+    description TEXT         NOT NULL,
+    issue_id    INT(6),
+    created_by  VARCHAR(100) NOT NULL,
+    created_at  TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_by  VARCHAR(100),
+    updated_at  TIMESTAMP    NOT NULL DEFAULT now()
+);
